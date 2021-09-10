ddc35c6adbb1e54a25b00566e30212baf59edab6
Wonchan Lee <wonchanl@nvidia.com>
2021-09-10 14:05:37 -0700

    Minor improvement for the identity mapping check

--- legate/core/store.py
+++ legate/core/store.py
@@ -725,7 +725,7 @@ class Store(object):
 
     def _compute_projection(self, partition):
         dims = self._invert_dimensions(tuple(range(self.ndim)))
-        if dims == tuple(range(self.ndim)):
+        if all(idx == dim for idx, dim in enumerate(dims)):
             return 0
         else:
             return self._runtime.get_projection(self.ndim, dims)
