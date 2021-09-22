2d81af479302dbc88337f1fa63cbb3b70d883728
Wonchan Lee <wonchanl@nvidia.com>
2021-09-22 15:22:52 -0700

    Make type registration calls return the dtype objects

--- legate/core/types.py
+++ legate/core/types.py
@@ -185,6 +185,7 @@ class TypeSystem(object):
             raise KeyError(f"{ty} is already in this type system")
         dtype = _Dtype(ty, size_in_bytes, code)
         self._types[dtype] = dtype
+        return dtype
 
     def make_alias(self, alias, src_type, copy_reduction_ops=True):
         dtype = self[src_type]
@@ -192,6 +193,7 @@ class TypeSystem(object):
         if copy_reduction_ops:
             dtype.copy_all_reduction_ops(copy)
         self._types[alias] = copy
+        return copy
 
     def __str__(self):
         return str(self._types)
