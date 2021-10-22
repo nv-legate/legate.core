d7ec4e75897c1c5943a56d2253c62f82811aa396
Mike Bauer <mike@lightsighter.org>
2021-10-22 13:22:44 -0700

    Add a nice error message for the case where users invoke legate outside of legion_python

--- legate/core/__init__.py
+++ legate/core/__init__.py
@@ -15,6 +15,17 @@
 
 from __future__ import absolute_import, division, print_function
 
+# Perform a check to see if we're running inside of Legion Python
+# If we're not then we should raise an error message
+using_legion_python = True
+try:
+    from legion_cffi import ffi, lib as legion
+except ModuleNotFoundError:
+    using_legion_python = False
+if not using_legion_python:
+    raise RuntimeError(
+        'All Legate programs must be run with a legion_python interperter. We recommend that you use the Legate driver script "bin/legate" found in the installation directory to launch Legate programs as it provides easy-to-use flags for invoking legion_python. You can see options for using the driver script with "bin/legate --help". You can also invoke legion_python directly. Use "bin/legate --verbose ..." to see some examples of how to call legion_python directly.'
+    )
 # Import select types for Legate library construction
 from legate.core.context import ResourceConfig
 from legate.core.legate import (
@@ -87,7 +98,6 @@ from legate.core.types import (
     complex128,
     ReductionOp,
 )
-from legion_cffi import ffi, lib as legion
 
 # Import the PyArrow type system
 from pyarrow import (
