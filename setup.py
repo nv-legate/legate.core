dab4855328d30ab9fdb953e516813c5dc2eaa88a
Marcin Zalewski <mzalewski@nvidia.com>
2022-02-07 18:35:49 -0800

    Update version for new develop branch

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ if args.recurse:
     sys.argv.remove("--recurse")
     setup(
         name="legate.core",
-        version="0.1",
+        version="22.03",
         packages=["legate", "legate.core", "legate.timing"],
         cmdclass={"build_py": my_build_py},
     )
