eb1cd687d067bcb0484570db9761e5534c2d66a7
Bryan Van de Ven <bvandeven@nvidia.com>
2022-03-08 18:26:20 -0800

    mypy / legate.core.types

---
+++ typings/legion_cffi/lib.pyi
@@ -0,0 +1,42 @@
+# Copyright 2021-2022 NVIDIA Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LEGION_REDOP_BASE: int
+LEGION_TYPE_TOTAL: int
+
+LEGION_REDOP_KIND_SUM: int
+LEGION_REDOP_KIND_DIFF: int
+LEGION_REDOP_KIND_PROD: int
+LEGION_REDOP_KIND_DIV: int
+LEGION_REDOP_KIND_MAX: int
+LEGION_REDOP_KIND_MIN: int
+LEGION_REDOP_KIND_OR: int
+LEGION_REDOP_KIND_AND: int
+LEGION_REDOP_KIND_XOR: int
+
+LEGION_TYPE_BOOL: int
+LEGION_TYPE_INT8: int
+LEGION_TYPE_INT16: int
+LEGION_TYPE_INT32: int
+LEGION_TYPE_INT64: int
+LEGION_TYPE_UINT8: int
+LEGION_TYPE_UINT16: int
+LEGION_TYPE_UINT32: int
+LEGION_TYPE_UINT64: int
+LEGION_TYPE_FLOAT16: int
+LEGION_TYPE_FLOAT32: int
+LEGION_TYPE_FLOAT64: int
+LEGION_TYPE_COMPLEX64: int
+LEGION_TYPE_COMPLEX128: int
