587c5dd62328be8cbe7b1dfe1dfeee9563823764
Marcin Zalewski <mzalewski@nvidia.com>
2022-03-28 18:30:52 -0700

    Fix the version of Legion to use in release

--- install.py
+++ install.py
@@ -205,7 +205,7 @@ def install_gasnet(gasnet_dir, conduit, thread_count):
     shutil.rmtree(temp_dir)
 
 
-def install_legion(legion_src_dir, branch, commit=None):
+def install_legion(legion_src_dir, branch, commit="24437c29"):
     print("Legate is installing Legion into a local directory...")
     # For now all we have to do is clone legion since we build it with Legate
     git_clone(
@@ -225,7 +225,7 @@ def install_thrust(thrust_dir):
     )
 
 
-def update_legion(legion_src_dir, branch, commit=None):
+def update_legion(legion_src_dir, branch, commit="24437c29"):
     # Make sure we are on the right branch for single/multi-node
     git_update(legion_src_dir, branch=branch, commit=commit)
 
@@ -976,7 +976,7 @@ def driver():
     )
     parser.add_argument(
         "--legion-branch",
-        dest="legion_branch",
+        dest=None,
         required=False,
         default="control_replication",
         help="Legion branch to build Legate with.",
