abbf9722766fcf0d32b9c358c8245563db164eac
Bryan Van de Ven <bvandeven@nvidia.com>
2022-04-07 12:27:21 -0700

    add timing modules

--- legate/timing/timing.py
+++ legate/timing/timing.py
@@ -15,33 +15,37 @@
 from __future__ import annotations
 
 import struct
+from typing import TYPE_CHECKING, Any, Optional, Type, Union
 
 import numpy
 
-from legate.core import Future, get_legion_context, get_legion_runtime, legion
+from ..core import Future, get_legion_context, get_legion_runtime, legion
+
+if TYPE_CHECKING:
+    import pyarrow
 
 
 class TimingRuntime(object):
-    def __init__(self):
+    def __init__(self) -> None:
         self.runtime = get_legion_runtime()
         self.context = get_legion_context()
 
-    def issue_execution_fence(self):
+    def issue_execution_fence(self) -> None:
         legion.legion_runtime_issue_execution_fence(self.runtime, self.context)
 
-    def measure_seconds(self):
+    def measure_seconds(self) -> Future:
         return Future(
             legion.legion_issue_timing_op_seconds(self.runtime, self.context)
         )
 
-    def measure_microseconds(self):
+    def measure_microseconds(self) -> Future:
         return Future(
             legion.legion_issue_timing_op_microseconds(
                 self.runtime, self.context
             )
         )
 
-    def measure_nanoseconds(self):
+    def measure_nanoseconds(self) -> Future:
         return Future(
             legion.legion_issue_timing_op_nanoseconds(
                 self.runtime, self.context
@@ -50,35 +54,39 @@ class TimingRuntime(object):
 
 
 class Time(object):
-    def __init__(self, future, dtype):
+    def __init__(self, future: Future, dtype: Any) -> None:
         self.future = future
         self.dtype = dtype
-        self.value = None
+        self.value: Union[int, float, None] = None
 
     @property
-    def __legate_data_interface__(self):
-        result = {"version": 1, "data": dict()}
+    def __legate_data_interface__(self) -> dict[str, Any]:
+        result: dict[str, Any] = {"version": 1, "data": dict()}
         result["data"]["Legate Timestamp"] = self
         return result
 
     @property
-    def type(self):
+    def type(self) -> Any:
         return self.dtype
 
     @property
-    def kind(self):
+    def kind(self) -> Type[Future]:
         return Future
 
     @property
-    def storage(self):
+    def storage(self) -> Union[int, float, None]:
         return self.value
 
     @property
-    def stores(self):
+    def stores(self) -> list[Any]:
         return [None, self]
 
     @staticmethod
-    def from_stores(type, stores, children=None):
+    def from_stores(
+        type: pyarrow.DataType,
+        stores: list[Any],
+        children: Optional[Any] = None,
+    ) -> Time:
         if len(stores) == 2:
             raise ValueError(f"Invalid store count: {len(stores)}")
         if stores[0] is not None:
@@ -86,43 +94,43 @@ class Time(object):
         return Time(stores[1], numpy.dtype(type.to_pandas_dtype()))
 
     @property
-    def region(self):
-        raise None
+    def region(self) -> None:
+        raise RuntimeError()
 
-    def __int__(self):
+    def __int__(self) -> int:
         return int(self.get_value())
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.get_value())
 
-    def __float__(self):
+    def __float__(self) -> float:
         return float(self.get_value())
 
-    def __add__(self, rhs):
+    def __add__(self, rhs: Union[int, float]) -> Union[int, float]:
         return self.get_value() + rhs
 
-    def __radd__(self, lhs):
+    def __radd__(self, lhs: Union[int, float]) -> Union[int, float]:
         return lhs + self.get_value()
 
-    def __sub__(self, rhs):
+    def __sub__(self, rhs: Union[int, float]) -> Union[int, float]:
         return self.get_value() - rhs
 
-    def __rsub__(self, lhs):
+    def __rsub__(self, lhs: Union[int, float]) -> Union[int, float]:
         return lhs - self.get_value()
 
-    def __mul__(self, rhs):
+    def __mul__(self, rhs: Union[int, float]) -> Union[int, float]:
         return self.get_value() * rhs
 
-    def __rmul__(self, lhs):
+    def __rmul__(self, lhs: Union[int, float]) -> Union[int, float]:
         return lhs * self.get_value()
 
-    def __div__(self, rhs):
+    def __div__(self, rhs: Union[int, float]) -> float:
         return self.get_value() / rhs
 
-    def __rdiv__(self, lhs):
+    def __rdiv__(self, lhs: Union[int, float]) -> float:
         return lhs / self.get_value()
 
-    def get_value(self):
+    def get_value(self) -> Union[int, float]:
         if self.value is None:
             if self.dtype == numpy.int64:
                 self.value = struct.unpack_from(
@@ -139,7 +147,7 @@ class Time(object):
 _timing = TimingRuntime()
 
 
-def time(units="us"):
+def time(units: str = "us") -> Time:
     # Issue a Legion execution fence and then perform a timing operation
     # immediately after it
     _timing.issue_execution_fence()
