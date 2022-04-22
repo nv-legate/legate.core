772a75144e11b8a4d20ade4b5caa035d936615c9
Wonchan Lee <wonchanl@nvidia.com>
2022-04-21 23:27:48 -0700

    Make sure we include stdio.h in the cuda helper (#211)

--- src/core/cuda/cuda_help.h
+++ src/core/cuda/cuda_help.h
@@ -18,6 +18,7 @@
 
 #include <assert.h>
 #include <cuda_runtime.h>
+#include <stdio.h>
 
 #define CHECK_CUDA(expr)                                      \
   do {                                                        \
