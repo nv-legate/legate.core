40cfe12c83edfd01052af3c30e31defcc47a1681
Wei Wu <eddy16112@gmail.com>
2022-04-28 16:33:52 -0700

    forget to update a file

--- src/core/comm/alltoall_thread_mpi.cc
+++ src/core/comm/alltoall_thread_mpi.cc
@@ -62,12 +62,12 @@ int collAlltoallMPI(const void *sendbuf, int sendcount, collDataType_t sendtype,
     recvfrom_mpi_rank = global_comm->mapping_table.mpi_rank[recvfrom_global_rank];
     assert(sendto_global_rank == global_comm->mapping_table.global_rank[sendto_global_rank]);
     assert(recvfrom_global_rank == global_comm->mapping_table.global_rank[recvfrom_global_rank]);
-    // tag: seg idx + rank_idx
-    int send_tag = sendto_global_rank * 10000 + global_rank; // which dst seg it sends to (in dst rank)
-    int recv_tag = global_rank * 10000 + recvfrom_global_rank; // idx of current seg we are receving (in src/my rank)
+    // tag: seg idx + rank_idx + tag
+    int send_tag = collGenerateAlltoallTag(sendto_global_rank, global_rank, global_comm);
+    int recv_tag = collGenerateAlltoallTag(global_rank, recvfrom_global_rank, global_comm);
 #ifdef DEBUG_PRINT
-    printf("i: %d === global_rank %d, rank %d, tid %d, send %d to %d, send_tag %d, recv %d from %d, recv_tag %d\n", 
-      i, global_rank, global_comm.mpi_rank, global_comm.tid, 
+    printf("i: %d === global_rank %d, mpi rank %d, send %d to %d, send_tag %d, recv %d from %d, recv_tag %d\n", 
+      i, global_rank, global_comm->mpi_rank, 
       sendto_global_rank, sendto_mpi_rank, send_tag, recvfrom_global_rank, recvfrom_mpi_rank, recv_tag);
 #endif
     res = MPI_Sendrecv(src, sendcount, sendtype, sendto_mpi_rank, send_tag, dst, recvcount, recvtype, recvfrom_mpi_rank, recv_tag, global_comm->comm, &status);
@@ -78,14 +78,14 @@ int collAlltoallMPI(const void *sendbuf, int sendcount, collDataType_t sendtype,
 	for(int i = 0 ; i < total_size; i++) {
     char* src = (char*)sendbuf_tmp + i * sendtype_extent * sendcount;
     char* dst = (char*)recvbuf + i * recvtype_extent * recvcount;
-    dest_mpi_rank = i / global_comm.nb_threads;
-    int send_tag = i * 10000 + global_rank; // which seg it sends to
-    int recv_tag = global_rank * 10000 + i; // idx of current seg
+    dest_mpi_rank = global_comm->mapping_table.mpi_rank[i];
+    int send_tag = collGenerateAlltoallTag(i, global_rank, global_comm);
+    int recv_tag = collGenerateAlltoallTag(global_rank, i, global_comm);
 #ifdef DEBUG_PRINT
-    printf("i: %d === global_rank %d, rank %d, tid %d, send %d to %d, send_tag %d, recv %d from %d, recv_tag %d\n", 
-      i, global_rank, global_comm.mpi_rank, global_comm.tid, i, dest_mpi_rank, send_tag, i, dest_mpi_rank, recv_tag);
+    printf("i: %d === global_rank %d, mpi rank %d, send %d to %d, send_tag %d, recv %d from %d, recv_tag %d\n", 
+      i, global_rank, global_comm->mpi_rank, i, dest_mpi_rank, send_tag, i, dest_mpi_rank, recv_tag);
 #endif
-    res = MPI_Sendrecv(src, sendcount, sendtype, dest_mpi_rank, send_tag, dst, recvcount, recvtype, dest_mpi_rank, recv_tag, global_comm.comm, &status);
+    res = MPI_Sendrecv(src, sendcount, sendtype, dest_mpi_rank, send_tag, dst, recvcount, recvtype, dest_mpi_rank, recv_tag, global_comm->comm, &status);
     assert(res == MPI_SUCCESS);
 	}
 #else
@@ -93,25 +93,25 @@ int collAlltoallMPI(const void *sendbuf, int sendcount, collDataType_t sendtype,
   int dest_mpi_rank;
 	for(int i = 0 ; i < total_size; i++) {
     char* src = (char*)sendbuf_tmp + i * sendtype_extent * sendcount;
-    dest_mpi_rank = i / global_comm.nb_threads;
-    tag = i * 10000 + global_rank;
-    res = MPI_Send(src, sendcount, sendtype, dest_mpi_rank, tag, global_comm.comm);
+    dest_mpi_rank = global_comm->mapping_table.mpi_rank[i];
+    tag = collGenerateAlltoallTag(i, global_rank, global_comm);
+    res = MPI_Send(src, sendcount, sendtype, dest_mpi_rank, tag, global_comm->comm);
     assert(res == MPI_SUCCESS);
 #ifdef DEBUG_PRINT
-    printf("i: %d === global_rank %d, rank %d, tid %d, send %d to %d, send_tag %d\n", 
-      i, global_rank, global_comm.mpi_rank, global_comm.tid, i, dest_mpi_rank, tag);
+    printf("i: %d === global_rank %d, mpi rank %d, send %d to %d, send_tag %d\n", 
+      i, global_rank, global_comm->mpi_rank, i, dest_mpi_rank, tag);
 #endif
 	}
   
   for(int i = 0 ; i < total_size; i++) {
     char* dst = (char*)recvbuf + i * recvtype_extent * recvcount;
-    int dest_mpi_rank = i / global_comm.nb_threads;
-    tag = global_rank * 10000 + i;
+    dest_mpi_rank = global_comm->mapping_table.mpi_rank[i];
+    tag = collGenerateAlltoallTag(global_rank, i, global_comm);
 #ifdef DEBUG_PRINT
-    printf("i: %d === global_rank %d, rank %d, tid %d, recv %d from %d, recv_tag %d\n", 
-      i, global_rank, global_comm.mpi_rank, global_comm.tid, i, dest_mpi_rank, tag);
+    printf("i: %d === global_rank %d, mpi rank %d, recv %d from %d, recv_tag %d\n", 
+      i, global_rank, global_comm->mpi_rank, i, dest_mpi_rank, tag);
 #endif
-    res = MPI_Recv(dst, recvcount, recvtype, dest_mpi_rank, tag, global_comm.comm, &status);
+    res = MPI_Recv(dst, recvcount, recvtype, dest_mpi_rank, tag, global_comm->comm, &status);
     assert(res == MPI_SUCCESS);
 	}
 #endif
