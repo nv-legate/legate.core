5cbc71339449ca9f795aafeecd8089e6b82876f1
ptaylor <paul.e.taylor@me.com>
2022-06-20 11:06:54 -0700

    remove cutensor from build dependencies as it isn't used

--- legate/__init__.py
+++ legate/__init__.py
@@ -12,12 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from __future__ import annotations
-
-from pkgutil import extend_path
-
-__path__ = extend_path(__path__, __name__)
-
 from . import _version
 
 __version__ = _version.get_versions()["version"]
