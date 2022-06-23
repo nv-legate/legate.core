9778e14bb0409c6424d6afb9c9a22dedb9cbe847
Wei Wu <eddy16112@gmail.com>
2022-06-23 16:05:51 -0700

    clean up assert

--- src/core/comm/coll.cc
+++ src/core/comm/coll.cc
@@ -264,9 +264,9 @@ int collInit(int argc, char* argv[])
   CHECK_MPI(MPI_Comm_get_attr(MPI_COMM_WORLD, MPI_TAG_UB, &tag_ub, &flag));
   assert(flag);
   mpi_tag_ub = *tag_ub;
-  assert(mpi_comms.empty() == true);
+  assert(mpi_comms.empty());
 #else
-  assert(thread_comms.empty() == true);
+  assert(thread_comms.empty());
 #endif
   coll_inited = true;
   return CollSuccess;
@@ -286,7 +286,7 @@ int collFinalize()
     LEGATE_ABORT;
   }
 #else
-  for (ThreadComm& thread_comm : thread_comms) { assert(thread_comm.ready_flag == false); }
+  for (ThreadComm& thread_comm : thread_comms) { assert(!thread_comm.ready_flag); }
   thread_comms.clear();
 #endif
   return CollSuccess;
