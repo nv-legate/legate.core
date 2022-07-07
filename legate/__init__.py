1a26f06e3b6d67ecc18b209327be138020d3e44c
ptaylor <paul.e.taylor@me.com>
2022-07-07 10:42:01 -0700

    add __future__ imports back in

--- legate/__init__.py
+++ legate/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from __future__ import annotations
 from . import _version
 
 __version__ = _version.get_versions()["version"]
