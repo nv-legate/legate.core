3b07ab6ff6004bca411119f67a078ffb09634f1b
Wei Wu <eddy16112@gmail.com>
2022-07-29 13:39:23 -0700

    add jupyter config file for legate

---
+++ install_jupyter_kernel.py
@@ -0,0 +1,17 @@
+import sys
+import os
+legion_dir = "/scratch2/wwu/legion_python"
+sys.path.append(legion_dir)
+from jupyter_notebook.install import parse_args, driver
+
+if __name__ == '__main__':
+    try:
+      legate_dir = os.environ["LEGATE_DIR"]
+    except KeyError:
+      print("Please sepecify the legate installtion dir by set LEGATE_DIR")
+      sys.exit()
+    args, opts = parse_args()
+    args.json = "legate_jupyter.json"
+    args.legion_prefix = legate_dir
+    kernel_file_dir = legion_dir + "/" + "jupyter_notebook"
+    driver(args, opts, kernel_file_dir)
