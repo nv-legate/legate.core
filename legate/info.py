1613f1f6138d51370a5da36c054fcdf1ed83a2c5
Wei Wu <eddy16112@gmail.com>
2022-08-24 15:47:43 -0700

    move magic cmd files into legate such that it can be installed along with legate

--- legate/info.py
+++ legate/info.py
@@ -1,3 +1,19 @@
+# Copyright 2021-2022 NVIDIA Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# mypy: ignore-errors
 import json
 
 from IPython.core.magic import Magics, line_magic, magics_class
@@ -28,7 +44,7 @@ class LegateInfo(object):
                 else:
                     self.config_dict[key] = json_dict[key]["value"]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         out_str = ""
         for key, value in self.config_dict.items():
             out_str += cmd_dict[key] + ": " + str(value) + "\n"
@@ -42,10 +58,10 @@ _legate_info = LegateInfo("legate_jupyter.json")
 @magics_class
 class LegateInfoMagics(Magics):
     @line_magic
-    def legate_info(self, line):
+    def legate_info(self, line: str) -> None:
         print(_legate_info)
         # return line
 
 
-def load_ipython_extension(ipython):
+def load_ipython_extension(ipython) -> None:
     ipython.register_magics(LegateInfoMagics)
