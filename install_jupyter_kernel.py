3c388b4d3b5cfe3306d6e4b342b8809464f09ee7
Wei Wu <eddy16112@gmail.com>
2022-08-23 17:52:02 -0700

    fix format

--- install_jupyter_kernel.py
+++ install_jupyter_kernel.py
@@ -1,10 +1,6 @@
 import os
 import sys
 
-legion_dir = os.getcwd() + "/legion"
-sys.path.append(legion_dir)
-from jupyter_notebook.install import driver, parse_args
-
 if __name__ == "__main__":
     try:
         legate_dir = os.environ["LEGATE_DIR"]
@@ -13,9 +9,15 @@ if __name__ == "__main__":
             "Please specify the legate installation dir by setting LEGATE_DIR"
         )
         sys.exit(1)
-    args, opts = parse_args()
     script_dir = os.path.dirname(os.path.realpath(__file__))
+    legion_dir = script_dir + "/legion"
+    sys.path.append(legion_dir)
+    from jupyter_notebook.install import driver, parse_args
+
+    args, opts = parse_args()
     args.json = os.path.join(script_dir, "legate_jupyter.json")
     args.legion_prefix = legate_dir + "/bin"
     kernel_file_dir = legion_dir + "/" + "jupyter_notebook"
     driver(args, opts, kernel_file_dir)
+
+    # TODO: copy legate_info.py and json file into legate dir
