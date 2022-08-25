ebf9e021f9db33602a91833a373550cb61f2b9f7
Rohan Yadav <rohany@alumni.cmu.edu>
2022-08-25 15:23:35 -0400

    legate/core: add the ability to apply affine projections to domain parts

--- legate/core/solver.py
+++ legate/core/solver.py
@@ -421,8 +421,9 @@ class Partitioner:
                                 "Partitions constrained by multiple "
                                 "constraints are not supported yet"
                             )
-                    for unknown in c._rhs.unknowns():
-                        must_be_even.add(unknown)
+                    if not isinstance(c._rhs, Image):
+                        for unknown in c._rhs.unknowns():
+                            must_be_even.add(unknown)
                     dependent[c._lhs] = c._rhs
                 elif isinstance(c, Containment) and isinstance(
                     c._rhs, PartSym
@@ -437,8 +438,9 @@ class Partitioner:
                                 "Partitions constrained by multiple "
                                 "constraints are not supported yet"
                             )
-                    for unknown in c._lhs.unknowns():
-                        must_be_even.add(unknown)
+                    if not isinstance(c._lhs, Image):
+                        for unknown in c._lhs.unknowns():
+                            must_be_even.add(unknown)
                     dependent[c._rhs] = c._lhs
                 else:
                     raise NotImplementedError
