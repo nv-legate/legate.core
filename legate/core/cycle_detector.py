17b3aa2f72f31c3d5a06f91b7db5234a37412b62
Manolis Papadakis <mpapadakis@nvidia.com>
2022-09-07 15:25:26 -0700

    WIP on cycle detection check

---
+++ legate/core/cycle_detector.py
@@ -0,0 +1,127 @@
+# Copyright 2022 NVIDIA Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import gc
+import inspect
+from collections import deque
+from typing import Any, Union
+
+from .store import RegionField
+
+
+def _find_cycles(root: Any, all_ids: set[int]) -> None:
+    opened: dict[int, int] = {}
+    closed: set[int] = set()
+    stack = [root]
+    while len(stack) > 0:
+        dst = stack[-1]
+        if id(dst) in opened:
+            if opened[id(dst)] == len(stack):
+                del opened[id(dst)]
+                closed.add(id(dst))
+            else:
+                print("found cycle!")
+                print("  tail:")
+                _bfs(dst, root, all_ids)
+                print("  cycle:")
+                _bfs(dst, dst, all_ids)
+                return
+            stack.pop()
+        elif id(dst) in closed:
+            stack.pop()
+        else:
+            opened[id(dst)] = len(stack)
+            for src in gc.get_referrers(dst):
+                if id(src) in all_ids:
+                    stack.append(src)
+
+
+def _find_field(src: Any, dst: Any) -> Union[str, None]:
+    if type(src) == dict:
+        for k, v in src.items():
+            if v is dst and isinstance(k, str):
+                return f'["{k}"]'
+    if type(src) == tuple:
+        for k, v in enumerate(src):
+            if v is dst:
+                return f"[{k}]"
+    try:
+        for fld in dir(src):
+            try:
+                if hasattr(src, fld) and getattr(src, fld) is dst:
+                    return "." + fld
+            except Exception:
+                pass
+    except Exception:
+        pass
+    try:
+        for fld in vars(src):
+            try:
+                if hasattr(src, fld) and getattr(src, fld) is dst:
+                    return "." + fld
+            except Exception:
+                pass
+    except Exception:
+        pass
+    try:
+        for fld, val in inspect.getmembers(src):
+            if val is dst:
+                return "." + fld
+    except Exception:
+        pass
+    return None
+
+
+def _bfs(begin: Any, end: Any, all_ids: set[int]) -> None:
+    parent = {}
+    q = deque([begin])
+    while len(q) > 0:
+        src = q.popleft()
+        for dst in gc.get_referents(src):
+            if id(dst) not in all_ids or id(dst) in parent:
+                continue
+            parent[id(dst)] = src
+            if dst is end:
+                print(f"    {hex(id(dst))}: {type(dst)}")
+                while True:
+                    src = parent[id(dst)]
+                    fld = _find_field(src, dst)
+                    if fld is None:
+                        print("     ^")
+                    else:
+                        print(f"     ^ {fld}")
+                    print(f"    {hex(id(src))}: {type(src)}")
+                    dst = src
+                    if dst is begin:
+                        break
+                return
+            q.append(dst)
+    print(f"    {hex(id(end))}: {type(end)}")
+    print("     ^")
+    print("    ???")
+    print("     ^")
+    print(f"    {hex(id(begin))}: {type(begin)}")
+
+
+def print_cycles() -> None:
+    all_objs = gc.get_objects()
+    all_ids = set(id(obj) for obj in all_objs)
+    for obj in all_objs:
+        if isinstance(obj, RegionField):
+            print(
+                f"looking for cycles involving {hex(id(obj))}, "
+                f"of type {type(obj)}"
+            )
+            _find_cycles(obj, all_ids)
