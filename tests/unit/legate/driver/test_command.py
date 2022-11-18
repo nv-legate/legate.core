70a15b2441c006465e828b8fefeef745ac940423
Manolis Papadakis <mpapadakis@nvidia.com>
2022-11-18 09:23:20 -0800

    Don't load a Realm network module if running on 1 rank

--- tests/unit/legate/driver/test_command.py
+++ tests/unit/legate/driver/test_command.py
@@ -58,6 +58,7 @@ def test_CMD_PARTS() -> None:
         m.cmd_numamem,
         m.cmd_fbmem,
         m.cmd_regmem,
+        m.cmd_network,
         m.cmd_log_levels,
         m.cmd_log_file,
         m.cmd_eager_alloc,
