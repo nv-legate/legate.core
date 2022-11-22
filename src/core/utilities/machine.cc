16624698a25e0d3f70c4d76e432987510b1d7027
Wonchan Lee <wonchanl@nvidia.com>
2022-11-21 21:10:10 -0800

    Update create_buffer to use socket memories whenever available (#487)

--- src/core/utilities/machine.cc
+++ src/core/utilities/machine.cc
@@ -16,15 +16,29 @@
 
 #include "core/utilities/machine.h"
 
+#include "core/runtime/runtime.h"
+#include "legate_defines.h"
+
 using namespace Legion;
 
 namespace legate {
 
-Memory::Kind find_memory_kind_for_executing_processor()
+Memory::Kind find_memory_kind_for_executing_processor(bool host_accessible)
 {
   auto proc = Processor::get_executing_processor();
-  return proc.kind() == Processor::Kind::TOC_PROC ? Memory::Kind::Z_COPY_MEM
-                                                  : Memory::Kind::SYSTEM_MEM;
+  switch (proc.kind()) {
+    case Processor::Kind::LOC_PROC: {
+      return Memory::Kind::SYSTEM_MEM;
+    }
+    case Processor::Kind::TOC_PROC: {
+      return host_accessible ? Memory::Kind::Z_COPY_MEM : Memory::Kind::GPU_FB_MEM;
+    }
+    case Processor::Kind::OMP_PROC: {
+      return Core::has_socket_mem ? Memory::Kind::SOCKET_MEM : Memory::Kind::SYSTEM_MEM;
+    }
+  }
+  LEGATE_ABORT;
+  return Memory::Kind::SYSTEM_MEM;
 }
 
 }  // namespace legate
