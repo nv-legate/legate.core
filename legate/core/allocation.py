0d877a745e916dd28aa4e6951dbb52fc1f20d957
Manolis Papadakis <manopapad@gmail.com>
2022-11-23 12:01:56 -0800

    Use the correct parameter name

--- legate/core/allocation.py
+++ legate/core/allocation.py
@@ -42,7 +42,9 @@ class InlineMappedAllocation:
 
     def __del__(self) -> None:
         if not self._consumed:
-            self._region_field.decrement_inline_mapped_ref_count(ordered=False)
+            self._region_field.decrement_inline_mapped_ref_count(
+                unordered=True
+            )
 
     def consume(
         self, ctor: Callable[[tuple[int, ...], int, tuple[int, ...]], Any]
