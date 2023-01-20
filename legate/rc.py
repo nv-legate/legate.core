95c26793ed223509ef239a46837e58bdaa556d51
Wei Wu <eddy16112@gmail.com>
2023-01-19 23:26:57 -0800

    fixup

--- legate/rc.py
+++ legate/rc.py
@@ -52,6 +52,5 @@ def has_legion_context() -> bool:
 
 def check_legion(msg: str = LEGION_WARNING) -> None:
     """Raise an error if we are not running in legion_python."""
-    pass
     if not has_legion_context():
         raise RuntimeError(msg)
