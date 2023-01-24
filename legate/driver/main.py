37a10baa7dc0772a1097edc66507569d74e50a69
Wei Wu <eddy16112@gmail.com>
2023-01-24 11:57:19 -0800

    fixup

--- legate/driver/main.py
+++ legate/driver/main.py
@@ -42,16 +42,7 @@ def prepare_driver(
     new_argv = argv.copy()
     for key, value in os.environ.items():
         if key in CONFIG_ENV_CMD:
-            try:
-                idx = new_argv.index(CONFIG_ENV_CMD[key])
-                assert new_argv[
-                    idx + 1
-                ].isnumeric(), "The argv %s %s is wrong" % (
-                    new_argv[idx],
-                    new_argv[idx + 1],
-                )
-                new_argv[idx + 1] = value
-            except ValueError:
+            if CONFIG_ENV_CMD[key] not in new_argv:
                 new_argv.append(CONFIG_ENV_CMD[key])
                 new_argv.append(value)
     print(new_argv)
