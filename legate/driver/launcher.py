878d4e70f2f4d9edbbae3bb47f877dd3f2ff7ea6
Wei Wu <eddy16112@gmail.com>
2023-01-23 16:10:59 -0800

    fixup

--- legate/driver/launcher.py
+++ legate/driver/launcher.py
@@ -81,7 +81,6 @@ class Launcher:
     def __init__(self, config: ConfigProtocol, system: System) -> None:
         self._config = config
         self._system = system
-        print(system.env)
 
         if config.multi_node.ranks == 1:
             self.detected_rank_id = "0"
