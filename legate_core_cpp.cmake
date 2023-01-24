1ce77597295fa86619b03e4f990dff0280dd994a
Manolis Papadakis <manopapad@gmail.com>
2023-01-23 20:31:44 -0800

    Use @loader_path instead of $ORIGIN on MacOS

--- legate_core_cpp.cmake
+++ legate_core_cpp.cmake
@@ -234,8 +234,6 @@ add_library(legate::core ALIAS legate_core)
 set_target_properties(legate_core
            PROPERTIES EXPORT_NAME                         core
                       LIBRARY_OUTPUT_NAME                 lgcore
-                      BUILD_RPATH                         "\$ORIGIN"
-                      INSTALL_RPATH                       "\$ORIGIN"
                       CXX_STANDARD                        17
                       CXX_STANDARD_REQUIRED               ON
                       CUDA_STANDARD                       17
@@ -243,6 +241,13 @@ set_target_properties(legate_core
                       POSITION_INDEPENDENT_CODE           ON
                       INTERFACE_POSITION_INDEPENDENT_CODE ON
                       LIBRARY_OUTPUT_DIRECTORY            lib)
+if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  set_target_properties(legate_core PROPERTIES BUILD_RPATH "\$ORIGIN")
+  set_target_properties(legate_core PROPERTIES INSTALL_RPATH "\$ORIGIN")
+elseif (CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+  set_target_properties(legate_core PROPERTIES BUILD_RPATH "@loader_path")
+  set_target_properties(legate_core PROPERTIES INSTALL_RPATH "@loader_path")
+endif ()
 
 # Add Conda library, and include paths if specified
 if(TARGET conda_env)
