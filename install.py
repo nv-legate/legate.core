3b5f69a372e23d26e574475663212ea8977bd17d
Marcin Zalewski <marcin.zalewski@gmail.com>
2023-01-30 14:14:04 -0800

    Fix the default Legion version (#547)
    Co-authored-by: Marcin Zalewski <mzalewski@nvidia.com>

--- install.py
+++ install.py
@@ -743,7 +743,7 @@ def driver():
         "--legion-branch",
         dest="legion_branch",
         required=False,
-        default="collective",
+        default="04cf06a2",
         help="Legion branch to build Legate with.",
     )
     args, unknown = parser.parse_known_args()
