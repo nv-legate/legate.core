561ad4c3749691b7ee978409802ac64cd5be36ae
Manolis Papadakis <manopapad@gmail.com>
2023-01-30 17:14:58 -0800

    Use @loader_path instead of $ORIGIN on MacOS (#543)
    * Use @loader_path instead of $ORIGIN on MacOS
    
    * Cleaner setting

--- legate_core_cpp.cmake
+++ legate_core_cpp.cmake
@@ -231,11 +231,17 @@ endif()
 add_library(legate_core ${legate_core_SOURCES})
 add_library(legate::core ALIAS legate_core)
 
+if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  set(platform_rpath_origin "\$ORIGIN")
+elseif (CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+  set(platform_rpath_origin "@loader_path")
+endif ()
+
 set_target_properties(legate_core
            PROPERTIES EXPORT_NAME                         core
                       LIBRARY_OUTPUT_NAME                 lgcore
-                      BUILD_RPATH                         "\$ORIGIN"
-                      INSTALL_RPATH                       "\$ORIGIN"
+                      BUILD_RPATH                         "${platform_rpath_origin}"
+                      INSTALL_RPATH                       "${platform_rpath_origin}"
                       CXX_STANDARD                        17
                       CXX_STANDARD_REQUIRED               ON
                       CUDA_STANDARD                       17
