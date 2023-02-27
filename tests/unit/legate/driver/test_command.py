33ba73792a7df6e60a34047dbf04e042d4ee4803
Manolis Papadakis <manopapad@gmail.com>
2023-02-27 15:52:46 -0800

    Fix unit test

--- tests/unit/legate/driver/test_command.py
+++ tests/unit/legate/driver/test_command.py
@@ -1184,7 +1184,7 @@ class Test_cmd_log_file:
         result = m.cmd_log_file(config, system, launcher)
 
         logfile = str(config.logging.logdir / "legate_%.log")
-        assert result == ("-logfile", logfile)
+        assert result == ("-logfile", logfile, "-errlevel", "4")
 
     def test_flag_with_dir(self, genobjs: GenObjs) -> None:
         config, system, launcher = genobjs(
@@ -1194,7 +1194,7 @@ class Test_cmd_log_file:
         result = m.cmd_log_file(config, system, launcher)
 
         logfile = str(Path("foo") / "legate_%.log")
-        assert result == ("-logfile", logfile)
+        assert result == ("-logfile", logfile, "-errlevel", "4")
 
 
 class Test_cmd_eager_alloc:
