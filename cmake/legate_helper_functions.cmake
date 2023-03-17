147d91a037c9e7d6abcf2b8ccea3c3a5433d6c86
jjwilke <jwilke@nvidia.com>
2023-03-17 14:23:33 -0700

    add license files to examples and generated content

--- cmake/legate_helper_functions.cmake
+++ cmake/legate_helper_functions.cmake
@@ -1,3 +1,19 @@
+#=============================================================================
+# Copyright 2023 NVIDIA Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#=============================================================================
+
 macro(legate_include_rapids)
   if (NOT _LEGATE_HAS_RAPIDS)
     if(NOT EXISTS ${CMAKE_BINARY_DIR}/LEGATE_RAPIDS.cmake)
@@ -151,7 +167,6 @@ header: str = """
   else()
     # libraries are built in a common spot
     set(libdir ${CMAKE_BINARY_DIR}/legate_${target})
-    message("libdir to binary dir")
   endif()
   add_custom_target("generate_install_info_py" ALL
     COMMAND ${CMAKE_COMMAND}
@@ -255,6 +270,22 @@ endfunction()
 function(legate_cpp_library_template target output_sources_variable)
   set(file_template
 [=[
+/* Copyright 2023 NVIDIA Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 #pragma once
 
 #include "legate.h"
@@ -284,6 +315,22 @@ struct Task : public legate::LegateTask<T> {
 
   set(file_template
 [=[
+/* Copyright 2023 NVIDIA Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 #include "legate_library.h"
 #include "core/mapping/mapping.h"
 
@@ -386,6 +433,21 @@ endfunction()
 function(legate_python_library_template target)
 set(file_template
 [=[
+# Copyright 2023 NVIDIA Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 from legate.core import (
     Library,
     ResourceConfig,
