21d71f746a1ec7d629368d9e199ed673390440df
Manolis Papadakis <manopapad@gmail.com>
2023-04-11 13:42:09 -0700

    Use parent frame indiscriminately in nested provenance (#666)
    * Use parent frame indiscriminately in nested provenance
    
    * Use a better string on unknown provenance
    
    * Update legate/core/context.py
    
    Co-authored-by: Bryan Van de Ven <bryan@bokeh.org>
    
    ---------
    
    Co-authored-by: Bryan Van de Ven <bryan@bokeh.org>

--- legate/core/context.py
+++ legate/core/context.py
@@ -15,7 +15,6 @@
 from __future__ import annotations
 
 import inspect
-import traceback
 from typing import (
     TYPE_CHECKING,
     Any,
@@ -53,16 +52,11 @@ class AnyCallable(Protocol):
         ...
 
 
-def find_last_user_frame(libname: str) -> str:
-    for frame, _ in traceback.walk_stack(inspect.currentframe()):
-        if "__name__" not in frame.f_globals:
-            continue
-        if not any(
-            frame.f_globals["__name__"].startswith(prefix)
-            for prefix in (libname, "legate")
-        ):
-            break
-
+def caller_frameinfo() -> str:
+    frame = inspect.currentframe()
+    if frame is None or frame.f_back is None or frame.f_back.f_back is None:
+        return "<unknown>"
+    frame = frame.f_back.f_back
     return f"{frame.f_code.co_filename}:{frame.f_lineno}"
 
 
@@ -350,7 +344,7 @@ class Context:
         if nested:
 
             def wrapper(*args: Any, **kwargs: Any) -> Any:
-                self.push_provenance(find_last_user_frame(self._libname))
+                self.push_provenance(caller_frameinfo())
                 result = func(*args, **kwargs)
                 self.pop_provenance()
                 return result
@@ -359,7 +353,7 @@ class Context:
 
             def wrapper(*args: Any, **kwargs: Any) -> Any:
                 if self.provenance is None:
-                    self.set_provenance(find_last_user_frame(self._libname))
+                    self.set_provenance(caller_frameinfo())
                     result = func(*args, **kwargs)
                     self.reset_provenance()
                 else:
