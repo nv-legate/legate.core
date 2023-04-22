d756ff9598def927e65b8b579f736033f4674b21
Manolis Papadakis <manopapad@gmail.com>
2023-04-21 17:23:39 -0700

    Use std::set.rbegin() instead of non-existent std::std.back() (#701)

--- src/core/mapping/base_mapper.cc
+++ src/core/mapping/base_mapper.cc
@@ -527,7 +527,10 @@ void BaseMapper::map_task(const Legion::Mapping::MapperContext ctx,
 #ifdef DEBUG_LEGATE
   assert(mapped_futures.size() <= task.futures.size());
   // The launching code should be packing all Store-backing Futures first.
-  if (mapped_futures.size() > 0) assert(mapped_futures.size() == mapped_futures.back() + 1);
+  if (mapped_futures.size() > 0) {
+    uint32_t max_mapped_fut = *mapped_futures.rbegin();
+    assert(mapped_futures.size() == max_mapped_fut + 1);
+  }
 #endif
 
   // Map future-backed stores
