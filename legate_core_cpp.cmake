4e356b66a84c15deb330c051915658a1c496efe5
Jeremy <jwilke@nvidia.com>
2023-06-05 15:04:26 -0700

    more type-o fixes

--- legate_core_cpp.cmake
+++ legate_core_cpp.cmake
@@ -464,9 +464,9 @@ Imported Targets:
 ]=])
 
 file(READ ${CMAKE_CURRENT_SOURCE_DIR}/cmake/legate_helper_functions.cmake helper_functions)
-file(READ ${CMAKE_CURRENT_SOURCE_DIR}/cmake/tmpl/cpp_source_template cpp_source_template)
-file(READ ${CMAKE_CURRENT_SOURCE_DIR}/cmake/tmpl/cpp_header_template cpp_header_template)
-file(READ ${CMAKE_CURRENT_SOURCE_DIR}/cmake/tmpl/python_template python_template)
+file(READ ${CMAKE_CURRENT_SOURCE_DIR}/legate/cpp_source_template cpp_source_template)
+file(READ ${CMAKE_CURRENT_SOURCE_DIR}/legate/cpp_header_template cpp_header_template)
+file(READ ${CMAKE_CURRENT_SOURCE_DIR}/legate/python_template python_template)
 
 string(JOIN "\n" code_string
 [=[
