ac75ac05a9056f49729797415ee489b45686a528
Mark Vaz <m3vaz@users.noreply.github.com>
2023-07-11 14:49:36 -0700

    Pin host compilers to 11.* during environment generation (#791)
    * pin host compilers to 11.*
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- scripts/generate-conda-envs.py
+++ scripts/generate-conda-envs.py
@@ -71,17 +71,18 @@ class CUDAConfig(SectionConfig):
             "pynvml",  # tests
         )
 
-        # gcc 11.3 is incompatible with nvcc <= 11.5.
-        if (
-            self.compilers
-            and self.os == "linux"
-            and (V(self.ctk_version) <= V("11.5"))
-        ):
-            deps += (
-                "gcc_linux-64<=11.2",
-                "gxx_linux-64<=11.2",
-            )
-
+        if self.compilers and self.os == "linux":
+            # gcc 11.3 is incompatible with nvcc <= 11.5.
+            if V(self.ctk_version) <= V("11.5"):
+                deps += (
+                    "gcc_linux-64<=11.2",
+                    "gxx_linux-64<=11.2",
+                )
+            else:
+                deps += (
+                    "gcc_linux-64=11.*",
+                    "gxx_linux-64=11.*",
+                )
         return deps
 
     def __str__(self) -> str:
