7d2344fbf605f78373ad115a19fc25d375f1a41f
ptaylor <paul.e.taylor@me.com>
2023-08-18 12:18:37 -0700

    update devcontainer versions

--- scripts/launch-devcontainer-conda.sh
+++ scripts/launch-devcontainer-conda.sh
@@ -6,5 +6,5 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..";
 if test ${#@} -le 0; then set -- bash -li; fi
 
 exec ./scripts/launch-devcontainer.sh \
-    rapidsai/devcontainers:23.08-cpp-cuda11.8-mambaforge-ubuntu22.04 \
+    rapidsai/devcontainers:23.10-cpp-cuda11.8-mambaforge-ubuntu22.04 \
     "${@}";
--- scripts/launch-devcontainer-pip.sh
+++ scripts/launch-devcontainer-pip.sh
@@ -6,5 +6,5 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..";
 if test ${#@} -le 0; then set -- bash -li; fi
 
 exec ./scripts/launch-devcontainer.sh \
-    rapidsai/devcontainers:23.08-cpp-rust-cuda11.8-ubuntu22.04 \
+    rapidsai/devcontainers:23.10-cpp-rust-cuda11.8-ubuntu22.04 \
     "${@}";
