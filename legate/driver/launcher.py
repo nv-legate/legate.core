a4b5430ebb2c52e3f8da8f27534bc0db8826b804
Manolis Papadakis <manopapad@gmail.com>
2023-10-31 21:42:57 -0700

    Remove extraneous PYTHONPATH entry (#878)

--- legate/driver/launcher.py
+++ legate/driver/launcher.py
@@ -15,7 +15,6 @@
 from __future__ import annotations
 
 import os
-from pathlib import Path
 from typing import TYPE_CHECKING
 
 from .. import install_info
@@ -173,9 +172,6 @@ class Launcher:
             extra_python_paths.append(
                 str(system.legion_paths.legion_jupyter_module)
             )
-
-        # Make sure the base directory for this file is in the python path
-        extra_python_paths.append(str(Path(__file__).parents[1]))
 
         env["PYTHONPATH"] = os.pathsep.join(extra_python_paths)
 
