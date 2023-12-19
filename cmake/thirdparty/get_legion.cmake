b258adab74f3e96bc071f8067bb5c6f9f96ca3f7
Manolis Papadakis <manopapad@gmail.com>
2023-12-18 22:57:19 -0800

    Fix build of rust profiler

--- cmake/thirdparty/get_legion.cmake
+++ cmake/thirdparty/get_legion.cmake
@@ -121,8 +121,6 @@ function(find_or_configure_legion)
       set(Legion_BACKTRACE_USE_LIBDW OFF)
     endif()
 
-    set(Legion_BUILD_RUST_PROFILER OFF CACHE BOOL "Whether to build the Legion profiler" FORCE)
-
     rapids_cpm_find(Legion ${version} ${FIND_PKG_ARGS}
         CPM_ARGS
           ${legion_cpm_git_args}
